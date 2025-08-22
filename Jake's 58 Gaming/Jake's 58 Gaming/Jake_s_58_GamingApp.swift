f518b658d9babe4e496ec68fae016cdb7fbf7d9d
Dias <dias.atudinov@gmail.com>
2025-08-22 17:38:51 +0500

    app init settings

---
+++ Jake's 58 Gaming/Jake's 58 Gaming/Jake_s_58_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jake_s_58_GamingApp.swift
+//  Jake's 58 Gaming
+//
+//  Created by Dias Atudinov on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Jake_s_58_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
