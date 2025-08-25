bda801ec72502f24e40b8cb3de31da13b810c162
Dias <dias.atudinov@gmail.com>
2025-08-25 17:26:36 +0500

    add settings

--- Jake's 58 Gaming/Jake's 58 Gaming/JGModules/JGSettings/JGSettingsView.swift
+++ Jake's 58 Gaming/Jake's 58 Gaming/JGModules/JGSettings/JGSettingsView.swift
@@ -2,14 +2,97 @@
 //  JGSettingsView.swift
 //  Jake's 58 Gaming
 //
-//  Created by Dias Atudinov on 25.08.2025.
 //
 
 import SwiftUI
 
 struct JGSettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var settingsVM = LEGSettingsViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack {
+                
+                Image(.settingsTextJG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: NGDeviceManager.shared.deviceType == .pad ? 140:75)
+                
+                HStack {
+                    
+                    ZStack {
+                        Image(.soundSettingsBgJG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NGDeviceManager.shared.deviceType == .pad ? 340:170)
+                        
+                        Button {
+                            withAnimation {
+                                settingsVM.soundEnabled.toggle()
+                            }
+                        } label: {
+                            Image(settingsVM.soundEnabled ? .onJG:.offJG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NGDeviceManager.shared.deviceType == .pad ? 200:110)
+                        }.offset(y: 10)
+                    }
+                    
+                    Image(.languageBgJG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: NGDeviceManager.shared.deviceType == .pad ? 500:300)
+                    
+                    ZStack {
+                        Image(.musicSettingsBgJG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NGDeviceManager.shared.deviceType == .pad ? 340:170)
+                        
+                        Button {
+                            withAnimation {
+                                settingsVM.musicEnabled.toggle()
+                            }
+                        } label: {
+                            Image(settingsVM.musicEnabled ? .onJG:.offJG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NGDeviceManager.shared.deviceType == .pad ? 200:110)
+                        }.offset(y: 10)
+                    }
+                }
+                Spacer()
+            }
+            
+            VStack {
+                HStack {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                        
+                    } label: {
+                        Image(.backIconJG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NGDeviceManager.shared.deviceType == .pad ? 100:50)
+                    }
+                    Spacer()
+                }.padding()
+                Spacer()
+                
+            }
+        }.frame(maxWidth: .infinity)
+            .background(
+                ZStack {
+                    Image(.appBgJG)
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                    
+                    
+                }
+                
+            )
     }
 }
 
--- Jake's 58 Gaming/Jake's 58 Gaming/JGModules/JGSettings/LEGSettingsViewModel.swift
+++ Jake's 58 Gaming/Jake's 58 Gaming/JGModules/JGSettings/LEGSettingsViewModel.swift
@@ -1,7 +1,15 @@
+//
+//  LEGSettingsViewModel.swift
+//  Jake's 58 Gaming
+//
+//  Created by Dias Atudinov on 25.08.2025.
+//
+
+
 import SwiftUI
 
 class LEGSettingsViewModel: ObservableObject {
     @AppStorage("soundEnabled") var soundEnabled: Bool = true
-    @AppStorage("brigthnessEnabled") var brigthnessEnabled: Bool = true
+    @AppStorage("musicEnabled") var musicEnabled: Bool = true
 
 }
